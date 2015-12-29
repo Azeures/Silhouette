1b17bd1d2ae903fa60175ae8d3dd4af89f78b182
Joel Jonas <Joel@196.203-211-151.static.qala.com.sg>
2015-12-29 14:15:20 +0800

    Initial Commit

---
+++ SilhouetteTests/SilhouetteTests.swift
@@ -0,0 +1,36 @@
+//
+//  SilhouetteTests.swift
+//  SilhouetteTests
+//
+//  Created by Joel Jonas on 29/12/15.
+//  Copyright © 2015 Joel Jonas. All rights reserved.
+//
+
+import XCTest
+@testable import Silhouette
+
+class SilhouetteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
